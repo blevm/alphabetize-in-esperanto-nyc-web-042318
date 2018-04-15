372b892a78404e1859650065c02d004f8dd75eb0
Michelle Blevins <mjblevins7@gmail.com>
2018-04-15 21:29:33 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   # code here
   esp_alph = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  ascii = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   arr.sort_by{|string| string.tr(esp_alph, ascii)}
 end
